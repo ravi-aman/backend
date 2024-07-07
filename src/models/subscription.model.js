2f5b93117a31a3f2429d30954c77f6ed19213356
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-07-07 18:10:31 +0530

    add cooment to subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,10 +1,14 @@
+// Importing mongoose and Schema from mongoose library
 import mongoose, { Schema } from "mongoose";
 
+// Defining the subscription schema using mongoose.Schema
 const subscriptionSchema = new mongoose.Schema({
+    // subscriber field to store the ID of the user who is subscribing
     subscriber: {
-        type: Schema.Types.ObjectId,//one who is  subscriber or subscribing
-        ref: "User"
+        type: Schema.Types.ObjectId, // Type of subscriber field is ObjectId
+        ref: "User" // Reference to the User model
     }
-}, { timeseries: true })
+}, { timeseries: true }) // Enabling timeseries option to store time-related data efficiently
 
+// Exporting the Subscription model based on the defined subscription schema
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
